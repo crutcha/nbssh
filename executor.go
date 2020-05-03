479704bf76ac973d89baeca067b33e091dc1b1b9
Andrew Crutchfield <andrewjcrutchfield@gmail.com>
2020-05-03 10:06:23 -0400

    trying coloring

--- executor.go
+++ executor.go
@@ -2,11 +2,36 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
+	//"encoding/json"
 	"fmt"
+	"golang.org/x/sys/unix"
+	"os"
 	"os/exec"
+	"strings"
 	"sync"
 )
+
+var terminalWidth uint16
+
+const (
+	InfoColor    = "\033[1;34m%s\033[0m"
+	NoticeColor  = "\033[1;36m%s\033[0m"
+	WarningColor = "\033[1;33m%s\033[0m"
+	ErrorColor   = "\033[1;31m%s\033[0m"
+	DebugColor   = "\033[0;36m%s\033[0m"
+)
+
+// TODO: what if its windows?
+func init() {
+	winSize, err := unix.IoctlGetWinsize(int(os.Stdout.Fd()), unix.TIOCGWINSZ)
+	terminalWidth = winSize.Col
+
+	if err != nil {
+		panic(err)
+	}
+
+	terminalWidth = winSize.Col
+}
 
 type Executor struct {
 	command     string
@@ -49,17 +74,25 @@ func (e *Executor) execute() {
 			var stdout, stderr bytes.Buffer
 			cmd.Stdout = &stdout
 			cmd.Stderr = &stderr
-			err := cmd.Run()
+			cmdErr := cmd.Run()
 
 			outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
-			result := Result{
-				CommandErr: fmt.Sprintf("%v", err),
-				Device:     device,
-				Stdout:     outStr,
-				Stderr:     errStr,
-			}
-			resultOut, _ := json.Marshal(result)
-			fmt.Println(string(resultOut))
+			/*
+				result := Result{
+					CommandErr: fmt.Sprintf("%v", err),
+					Device:     device,
+					Stdout:     outStr,
+					Stderr:     errStr,
+				}
+				resultOut, _ := json.Marshal(result)
+				fmt.Println(string(resultOut))
+			*/
+			fmt.Println(strings.Repeat("#", int(terminalWidth)))
+			fmt.Println(device)
+			fmt.Println(strings.Repeat("#", int(terminalWidth)))
+			fmt.Println(outStr)
+			fmt.Printf(InfoColor, errStr)
+			fmt.Println(cmdErr, "\n")
 
 			<-poolSemaphore
 		}(device)
--- go.mod
+++ go.mod
@@ -6,8 +6,11 @@ require (
 	github.com/alecthomas/kingpin v2.2.6+incompatible
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
+	github.com/buger/goterm v0.0.0-20200322175922-2f3e71b85129
 	github.com/go-openapi/runtime v0.19.14
 	github.com/go-openapi/strfmt v0.19.5
+	github.com/google/goterm v0.0.0-20190703233501-fc88cf888a3f
 	github.com/netbox-community/go-netbox v0.0.0-20200325111416-50e92f3e2076
 	golang.org/x/crypto v0.0.0-20190617133340-57b3e21c3d56
+	golang.org/x/sys v0.0.0-20190616124812-15dcb6c0061f
 )
